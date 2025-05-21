ced337c1f1883800ba23534f404d0244e9985de0
peerapat atawatana <broccolizz@gmail.com>
2025-05-21 10:53:10 +0700

    Initial Commit

---
+++ TestComposableArchitecture/TestComposableArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestComposableArchitectureApp.swift
+//  TestComposableArchitecture
+//
+//  Created by peerapat atawatana on 21/5/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct TestComposableArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestComposableArchitectureTests/TestComposableArchitectureTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestComposableArchitectureTests.swift
+//  TestComposableArchitectureTests
+//
+//  Created by peerapat atawatana on 21/5/2568 BE.
+//
+
+import Testing
+@testable import TestComposableArchitecture
+
+struct TestComposableArchitectureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
